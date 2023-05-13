712b8b143579ce2f28349e572504d5cda6cb9aed
Jan Szczepan <janszczepan62@gmail.com>
2023-05-13 16:10:13 +0200

    hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-deploy'
+import 'dotenv/config'
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ''
 
 const config: HardhatUserConfig = {
+   defaultNetwork: 'hardhat',
+   networks: {
+      hardhat: {
+         chainId: 31337,
+         forking: {
+            url: MAINNET_RPC_URL,
+         },
+      },
+   },
    solidity: '0.8.18',
+   namedAccounts: {
+      deployer: {
+         default: 0,
+         1: 0,
+      },
+   },
 }
 
 export default config
